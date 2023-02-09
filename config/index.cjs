fda44a662009d81f8477fe9ba70faf054498efe2
superO716 <93930646+superO716@users.noreply.github.com>
2023-02-10 00:12:37 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -49,7 +49,7 @@ const USER_CONFIG = {
 
 
   // 【推送完成提醒】模板id, 用来看自己有没有发送成功的那个模板
-  CALLBACK_TEMPLATE_ID: '	vox7VRkugvxhZ4bCOsEBw1falR8Wws7HvfsKU72lbO8',
+  CALLBACK_TEMPLATE_ID: 'vox7VRkugvxhZ4bCOsEBw1falR8Wws7HvfsKU72lbO8',
 
   CALLBACK_USERS: [
     {
